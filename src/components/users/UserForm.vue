59fd425ff29d2c2b5fe0c15c3e83f6404114b4f6
Pedro Henrique <phthedevx@gmail.com>
2025-06-01 11:28:10 -0300

    Cria estrutura inicial para cadastro e listagem de usuários

---
+++ src/components/users/UserForm.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h2>Cadastro de Usuário</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UserForm'
+  }
+  </script>
+  
---
+++ src/components/users/UserList.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h2>Lista de Usuários</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UserList'
+  }
+  </script>
+  
